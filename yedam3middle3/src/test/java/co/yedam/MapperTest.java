d2f2d7a26bdf4c4e9f8b46a77ab3f97a45f1b18f
admin <admin@DESKTOP-ERD09N0>
2024-03-20 17:42:22 +0900

    test delete

--- yedam3middle3/src/test/java/co/yedam/MapperTest.java
+++ yedam3middle3/src/test/java/co/yedam/MapperTest.java
@@ -3,9 +3,6 @@ package co.yedam;
 import java.util.List;
 
 import co.yedam.common.DataSource;
-import co.yedam.my.Qna;
-import co.yedam.my.SearchVO;
-import co.yedam.my.mapper.MyMapper;
 import co.yedam.member.mapper.MemberMapper;
 
 public class MapperTest {
@@ -13,12 +10,5 @@ public class MapperTest {
 
 		MemberMapper mapper = DataSource.getInstance().openSession(true).getMapper(MemberMapper.class);
 
-		SearchVO search = new SearchVO();
-		
-		List<Qna> list = mapper.qnaList(search);
-		for (Qna value : list) {
-			System.out.println(value);
 		}
 	}
-
-}
